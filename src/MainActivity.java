6f6d18e04e9e9ae617372e6b5875531bb058d44a
NirajMaharjan007 <niraj.mhrjn770@gmail.com>
2022-09-21 20:13:05 +0545

    Little change

--- src/MainActivity.java
+++ src/MainActivity.java
@@ -6,7 +6,6 @@ public class MainActivity {
     public static void main(String[] args) throws Exception {
         UIManager.setLookAndFeel("com.jtattoo.plaf.acryl.AcrylLookAndFeel");
         System.out.println("MainActivity.main()");
-	System.out.println("Hello World");
         new MainFrame();
     }
 }
