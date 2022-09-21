d960c9b72fbf1adcf6c52f43e42d87f4582ef383
NirajMaharjan007 <niraj.mhrjn770@gmail.com>
2022-09-21 20:00:51 +0545

    Theme changed

--- src/MainActivity.java
+++ src/MainActivity.java
@@ -4,7 +4,7 @@ import resources.MainFrame;
 
 public class MainActivity {
     public static void main(String[] args) throws Exception {
-        UIManager.setLookAndFeel("com.jtattoo.plaf.bernstein.BernsteinLookAndFeel");
+        UIManager.setLookAndFeel("com.jtattoo.plaf.acryl.AcrylLookAndFeel");
         System.out.println("MainActivity.main()");
         new MainFrame();
     }
